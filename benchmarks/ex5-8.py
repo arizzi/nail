602ae579ef71638c35831f27712f10ced99c6222
Andrea Rizzi <andrea.rizzi@cern.ch>
2023-02-24 14:29:25 +0100

    python3

--- benchmarks/ex5-8.py
+++ benchmarks/ex5-8.py
@@ -70,10 +70,10 @@ flow.binningRules = [
     (".*btag.*", "100,0,1"),
     (".*Mt.*", "100,0,1000")
 ]
-flow.printRDFCpp([], debug=False, outname="tmp.C",
-                 selections=histosPerSelection)
-print("code generated")
-os.system("g++  -fPIC -Wall -O3 tmp.C $(root-config --libs --cflags)  -o tmp")
-print("code compiled")
-os.system("./tmp 4")
-print("code run")
+
+processor = flow.CreateProcessor("eventProcessor", [], histosPerSelection, [], "", 4)
+rdf = ROOT.RDataFrame("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root")
+result = processor(rdf)
+for h in result.histos:
+        h.Draw()
+                                                                                                                                
